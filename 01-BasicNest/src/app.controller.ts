b414002912f18c3e1ef4c9971c773853cfb2647d
IdaDanuartha <danuart21@gmail.com>
2023-10-11 23:22:59 +0800

    learn basic nest from scratch done

---
+++ 01-BasicNest/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from "@nestjs/common"
+
+@Controller('/app')
+export class AppController {
+
+    @Get('/index')
+    getRootRoute() {
+        return 'hi there!';
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return 'bye there!';
+    }
+}
